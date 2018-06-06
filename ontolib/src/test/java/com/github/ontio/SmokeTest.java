7886bebe4b8c7e66fccfd393a99c59f8409154ea
chenjiefei <nlpx_dc@hotmail.com>
2018-06-06 17:34:01 +0800

    fix for gradle build

---
+++ ontolib/src/test/java/com/github/ontio/SmokeTest.java
@@ -0,0 +1,13 @@
+package com.github.ontio;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SmokeTest {
+    @Test
+    public void smoke(){
+        assertTrue(1+1 == 2);
+    }
+
+}
