2a1f90c38c71bc53b92641ba912da7c31ac7056d
lucas7788 <sishsh@163.com>
2020-04-19 12:22:48 +0800

    add wasmvm

---
+++ ontolib/src/main/java/com/github/ontio/core/payload/InvokeWasmCode.java
@@ -0,0 +1,33 @@
+package com.github.ontio.core.payload;
+
+import com.github.ontio.common.Address;
+import com.github.ontio.core.transaction.Transaction;
+import com.github.ontio.core.transaction.TransactionType;
+import com.github.ontio.io.BinaryReader;
+import com.github.ontio.io.BinaryWriter;
+
+import java.io.IOException;
+
+public class InvokeWasmCode extends Transaction {
+
+    public byte[] invokeCode;
+
+    public InvokeWasmCode(byte[] invokeCode) {
+        super(TransactionType.InvokeWasm);
+        this.invokeCode = invokeCode;
+    }
+
+    @Override
+    protected void deserializeExclusiveData(BinaryReader reader) throws IOException {
+        try {
+            invokeCode = reader.readVarBytes();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void serializeExclusiveData(BinaryWriter writer) throws IOException {
+        writer.writeVarBytes(invokeCode);
+    }
+}
